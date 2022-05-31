c0d7a24d454c4c8fa269c074550b182bad916b55
Arceg <101935035+Arceg@users.noreply.github.com>
2022-05-31 13:48:25 +0200

    Add files via upload

---
+++ xd/Upgrader_simulator.java
@@ -0,0 +1,332 @@
+import java.util.Random;
+import java.util.Scanner;
+class Upgrader_simulator {
+
+    public static boolean main(String[] args)    {
+        Scanner scan = new Scanner(System.in);
+        Random rnd = new Random();
+
+
+        //80%
+        int upsp = rndRange(100);
+
+        if (upsp == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80;)
+        {
+        System.out.println("Upgrade Succeed!");
+        Object scan;
+        return scan.nextline;
+    }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //70%
+
+        int upsp1 = rndRange(100);
+
+        if (upsp1 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70 )
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        // 60%
+
+        int upsp2 = rndRange(100);
+
+        if (upsp2 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60 )
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //50%
+
+        int upsp3 = rndRange(100);
+
+        if (upsp3 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50)
+        {
+
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //40%
+
+        int upsp4 = rndRange(100);
+
+        if (upsp4 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40;)
+
+    {
+        System.out.println("Upgrade Succeed!");
+        Object scan;
+        return scan.nextline;
+    }
+        else
+            {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //35%
+
+        int upsp5 = rndRange(100);
+
+        if (upsp5 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35 )
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //30%
+
+        int upsp6 = rndRange(100);
+
+        if (upsp6 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //25%
+
+        int upsp7 = rndRange(100);
+
+        if (upsp7 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //20%
+
+        int upsp8 = rndRange(100);
+
+        if (upsp8 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //10%
+
+        int upsp9 = rndRange(100);
+
+        if (upsp9 == 1,2,3,4,5,6,7,8,9,10)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //7%
+
+        int upsp10 = rndRange(100);
+
+        if (upsp10 == 1,2,3,4,5,6,7)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //5%
+
+        int upsp11 = rndRange(100);
+
+        if (upsp11 == 1,2,3,4,5)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //3%
+
+        int upsp12 = rndRange(100);
+
+        if (upsp12 == 1,2,3)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //1,5%
+
+        int upsp13 = rndRange(1000);
+
+        if (upsp13 == 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //1,2%
+
+        int upsp14 = rndRange(1000);
+
+        if (upsp14 == 1,2,3,4,5,6,7,8,9,10,11,12)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //1%
+
+        int upsp15 = rndRange(100);
+
+        if (upsp15 == 1)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //0,8%
+
+        int upsp16 = rndRange(1000);
+
+        if (upsp16 == 1,2,3,4,5,6,7,8)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //0,6%
+
+        int upsp17 = rndRange(1000);
+
+        if (upsp17 == 1,2,3,4,5,6)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //0,4%
+
+        int upsp18= rndRange(1000);
+
+        if (upsp18 == 1,2,3,4)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //0,2%
+
+        int upsp19= rndRange(1000);
+
+        if (upsp19 == 1,2)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+        //0,1%
+
+        int upsp20= rndRange(1000);
+
+        if (upsp20 == 1)
+        {
+            System.out.println("Upgrade Succeed!");
+            Object scan;
+            return scan.nextline;
+        }
+        else
+        {
+            System.out.println("Upgrade Failed!");
+        }
+
+
+
+
+
+
+
+
